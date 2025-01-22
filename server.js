a8af18d45563accdb8cbcf5d6b9cd7e3505d8426
Pratyek Thumula <pratyekpk3@gmail.com>
2025-01-22 16:03:34 +0530

    second

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const app = require("./app");
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const MONGO_URI =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI // Use Atlas in production
+    : "mongodb://localhost:27017/blogDB"; // Use local MongoDB in development
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI) // Removed deprecated options
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
     console.error("Database connection error:", err);
   });
-
